fe0a14b632da4111b5d57aa5a7e0361aed6d09f3
boaziza <b.iza@alustudent>
2025-08-01 10:22:30 -0700

    Y

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-"""Script that fetches https://alu-intranet.hbtn.io/status"""
+"""Script that fetches a status URL"""
 
 import urllib.request
+import sys
 
 if __name__ == "__main__":
-    url = "http://0.0.0.0:5050/status"
+    # Use command line argument if provided, otherwise use default
+    if len(sys.argv) > 1:
+        url = sys.argv[1]
+    else:
+        url = "https://alx-intranet.hbtn.io/status"
     
     with urllib.request.urlopen(url) as response:
         body = response.read()
